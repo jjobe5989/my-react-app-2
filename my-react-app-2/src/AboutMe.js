0d04ab3272223ff0d1963ba3b77988d9cf337da3
jjobe5989 <s3thjobe@gmail.com>
2023-03-15 19:22:46 -0500

    installed react and added AboutMe.js, Navigation.js, and Headers.js with some code.

---
+++ my-react-app-2/src/AboutMe.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AboutMe() {
+  return (
+    <section>
+      <h2>About Me</h2>
+      <p>Insert your bio here.</p>
+    </section>
+  );
+}
+
+export default AboutMe;
