89c8dda76eae47182e656a602b7801c8011f4f2a
jjobe5989 <s3thjobe@gmail.com>
2023-03-16 19:57:11 -0500

    added some more styling

--- my-react-app-2/src/AboutMe.js
+++ my-react-app-2/src/AboutMe.js
@@ -3,13 +3,30 @@ import './AboutMe.css';
 
 function AboutMe() {
   return (
-    <section>
-      <h2>About Me</h2>
-      <p>Hi, I am Jonathan and I am a Front-End focused web developer located in Middle Tennessee with a passion for design. Some more things about me are that I love to do automotive work, and I am a very hands on person.  
+    <div>
+      <section id="work" className="jobs">
+        <div className="flex-row">
+          <h2 className="section-title secondary-border">About Me</h2>
+            <div className="job-text">
+              <p>
+              Hi, I am Jonathan and I am a Front-End focused web developer located in Middle Tennessee with a passion for design. Some more things about me are that I love to do automotive work, and I am a very hands on person.
+              </p>
       
-      </p>
-    </section>
+            </div>
+          </div>
+      </section>
+    </div>
   );
 }
 
 export default AboutMe;
+
+// return (
+//   <div>
+//     <div className='AboutMe'>
+//     <h2>About Me</h2>
+//     <p>Hi, I am Jonathan and I am a Front-End focused web developer located in Middle Tennessee with a passion for design. Some more things about me are that I love to do automotive work, and I am a very hands on person.  
+//     </p>
+//   </div>
+//   </div>
+// );
